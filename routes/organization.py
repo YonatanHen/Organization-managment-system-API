e3fc57c92995c46497d7a21e1a2365fff7bd5ee0
YehonatanHen <yonatan.h333@gmail.com>
2024-09-26 18:18:59 +0300

    Created the delete org API + Added exepction to update org

--- routes/organization.py
+++ routes/organization.py
@@ -12,35 +12,42 @@ def add_organization():
 
             organization = create_organization(data['name'])
             
-            return jsonify(organization) 
+            return jsonify({"message": "Organization added successfully", "organization": organization}) 
             
         except KeyError:
             # Handle missing 'name' in the request payload
-            return jsonify({'error': 'The "name" field is required.'}), 400
-        
+            return jsonify({'error': 'The "name" field is required.'}), 400 
         except SQLAlchemyError as e:
             return jsonify({'error': 'Database error occurred.', 'message': str(e)}), 500
-
         except Exception as e:
             return jsonify({'error': 'An error occurred', 'message': str(e)}), 500
           
 
 @org_bp.route('/<int:id>', methods=['PUT', 'DELETE'])
-def update_or_delete_organization(id):
+def update_or_delete_organization(id: int):
     if request.method == 'PUT':
         try:
             data = request.get_json()
 
             organization = update_organization(id, data['name'])
             
-            return jsonify(organization)    
+            return jsonify({"message": "Organization updated successfully", "organization": organization})    
                  
         except KeyError:
             # Handle missing 'name' in the request payload
             return jsonify({'error': 'The "name" field is required.'}), 400
+        except SQLAlchemyError as e:
+            return jsonify({'error': 'Database error occurred.'}), 500
+        except Exception as e:
+            return jsonify({'error': 'An error occurred', 'message': str(e)}), 500            
+
+    elif request.method == 'DELETE':
+        try:
+            organization = delete_organization(id)
+            
+            return jsonify({"message": "Organization deleted successfully", "organization": organization}) 
         
         except SQLAlchemyError as e:
             return jsonify({'error': 'Database error occurred.'}), 500
-
         except Exception as e:
-            return jsonify({'error': 'An error occurred', 'message': str(e)}), 500            
+            return jsonify({'error': 'An error occurred', 'message': str(e)}), 500 
--- services/organization_service.py
+++ services/organization_service.py
@@ -31,7 +31,12 @@ def update_organization(id: int, new_name: str):
     organization = session.query(Organization).filter_by(id=id).first()
     
     if organization is None:
-        raise ValueError(f"No organization with id #{id} was found")
+        raise ValueError(f"Organization with id #{id} wasn't found.")
+    
+    org_name = session.query(Organization).filter_by(name=new_name).first()
+    
+    if org_name is not None:
+        raise ValueError(f"Organization '{new_name}' already exists.")
     
     organization.name = new_name
     
@@ -42,4 +47,21 @@ def update_organization(id: int, new_name: str):
     session.close()
     
     return res 
+
+def delete_organization(id: int):
+    session=get_db_session()
     
+    organization = session.query(Organization).filter_by(id=id).first()
+    
+    if organization is None:
+        raise ValueError(f"No organization with id #{id} was found")
+    
+    session.delete(organization)
+    
+    session.commit()
+    
+    res = {"id": organization.id, "name": organization.name}
+    
+    session.close()
+    
+    return res 
