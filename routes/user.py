365d81609d2151031f8ffdd42621d3172fa77d46
YehonatanHen <yonatan.h333@gmail.com>
2024-09-26 15:25:16 +0300

    Create User API initial setup

--- routes/user.py
+++ routes/user.py
@@ -1,3 +1,9 @@
 from flask import Flask, jsonify, Blueprint, request
 
 user_bp = Blueprint('user', __name__, url_prefix='/user')
+
+# @user_bp.route('/user', methods=['GET', 'POST', 'DELETE'])
+# def user(request):
+#     if request.method == 'POST':
+        
+    
