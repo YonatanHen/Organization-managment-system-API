cfb7b4b5244c79c000144f10c379afcfd6853dba
YehonatanHen <yonatan.h333@gmail.com>
2024-09-28 17:29:20 +0300

    Created env file

--- DB/psql_connection.py
+++ DB/psql_connection.py
@@ -1,15 +1,18 @@
 import os
 import sys
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
+
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from models import Base
 
-#In production environment we must keep credentials secure by enabling them in the activate script of the venv
-#In order to simplify the startup process in this task, I exposed the credentials.
-#PSQL_USERNAME=os.environ.get('PSQL_USERNAME')
-#PSQL_PASSWORD=os.environ.get('PSQL_PASSWORD')
-#PSQL_URL=os.environ.get('PSQL_URL')
+# Load environment variables from .env file
+load_dotenv()
 
-engine = create_engine(f"postgresql://postgres:password@localhost:5432/postgres")
+PSQL_USERNAME=os.environ.get('PSQL_USERNAME')
+PSQL_PASSWORD=os.environ.get('PSQL_PASSWORD')
+PSQL_URL=os.environ.get('PSQL_URL')
+
+engine = create_engine(f"postgresql://{PSQL_USERNAME}:{PSQL_PASSWORD}@{PSQL_URL}")
 Base.metadata.create_all(bind=engine)
