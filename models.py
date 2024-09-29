0d304deb364819ba8d19d567c9577b2d18d24f76
YehonatanHen <yonatan.h333@gmail.com>
2024-09-29 10:14:11 +0300

    removed autoincrement

--- models.py
+++ models.py
@@ -7,14 +7,14 @@ Base = declarative_base()
 class Organization(Base):
     __tablename__ = "organizations"
     
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(unique=True, nullable=False)
     endpoints: Mapped[List['Endpoint']] = relationship(backref='organization', cascade="all, delete-orphan")
     
 class Endpoint(Base):
     __tablename__ = "endpoints"
     
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(nullable=False)
     organization_id: Mapped[int] = mapped_column(ForeignKey('organizations.id', ondelete='CASCADE'))
     users: Mapped[List['User']] = relationship(backref='endpoint', cascade="all, delete-orphan")
@@ -26,6 +26,6 @@ class Endpoint(Base):
 class User(Base):
     __tablename__ = "users"
     
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(unique=True, nullable=False)
     endpoint_id: Mapped[int] = mapped_column(ForeignKey('endpoints.id', ondelete='CASCADE'))
